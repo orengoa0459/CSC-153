b2281939a9fa2ed46e5219c897dbe36ae66e75ef
orengoa0459 <48802353+orengoa0459@users.noreply.github.com>
2020-04-03 21:07:05 -0400

    Add files via upload

---
+++ M4HW1_OrengoAnthony/CarLibrary/Car.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLibrary
+{
+    public class Car
+    {
+        //Create private fields
+        private int _year;
+        private string _make;
+        private int _speed;
+
+        //Constructor
+        public Car(int year, string make, int speed)
+        {
+            Year = year;
+            Make = make;
+            Speed = speed;
+        }
+
+        //Set full properties for year, make, and speed
+        public int Year
+        {
+            get
+            {
+                return _year;
+            }
+            set
+            {
+               _year = value;
+            }
+
+        }
+        public string Make
+        {
+            get
+            {
+                return _make;
+            }
+            set
+            {
+                _make = value;
+            }
+        }
+        public int Speed
+        {
+            get
+            {
+                return _speed;
+            }
+
+            set
+            {
+                _speed = value;
+            }
+        }
+
+    }
+}
+      
+
