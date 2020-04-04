f595db0e67878f5ea55c252dc14eafff68a192af
orengoa0459 <48802353+orengoa0459@users.noreply.github.com>
2020-04-03 21:01:44 -0400

    Add files via upload

---
+++ ZombieAdventure/ConsoleUI/CreateCharacter.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameLibrary;
+namespace ConsoleUI
+{
+    public class CreateCharacter
+    {
+        public static Player player = new Player();
+        public static void CreateZombieAdvCharacter(Player player, GameRoom room)
+        {
+            //Sentinel to control loop
+            bool characterLoop = false;
+            string input;
+            Console.WriteLine(StandardMessages.CreateCharacterDirections());
+            Console.ReadLine();
+            Console.Clear();
+            do
+            {
+                //Character creation menu 
+                
+                Console.WriteLine(StandardMessages.DisplayCreateCharacterMenu());
+                input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        //Creat name and password
+                        CreateNameAndPassword(player, characterLoop);
+                        break;
+                    
+                    case "2": 
+                        //Select player class
+                        SelectPlayerClass(player, input, characterLoop);
+                        break;
+                    case "3":    
+                        //Select player race
+                        SelectPlayerRace(player, input, characterLoop);
+                        break;
+                    case "4":
+                        //Display created player information
+                        Console.WriteLine($"Name: {player.Name}\nPassword: {player.Password}\nCLass: {player.ClassType}\nRace: {player.Race}");
+                        Console.ReadLine();
+
+                        break;                   
+                    case "5":
+                        //Test to ensure player completes character creation 
+                        if (player.Name == "" || player.Password == "" || player.ClassType == "" || player.Race == "") 
+                        {
+                            Console.WriteLine("You must complete all tasks to proceed!");
+                            Console.ReadLine();
+                        }
+                        else
+                        {
+                            characterLoop = true;
+                            PlayerMovement.CharacterMovement(player, room);
+                        }
+                        break;
+                    default:
+                        Console.WriteLine(StandardMessages.InvalidSelection());
+                        break;
+                }
+                Console.Clear();
+            } while (characterLoop == false);            
+        }
+
+        public static void CreateNameAndPassword(Player player, bool characterLoop)
+        {
+            //Create user name 
+            Console.Write("Enter your name ==> ");
+            string playerName = Console.ReadLine();
+            player.Name = playerName;
+
+            do
+            {
+                //Create Password
+                Console.WriteLine(StandardMessages.DisplayPasswordCreationInformation());
+                Console.Write("Enter your Password ==> ");
+                string passWord1 = Console.ReadLine();
+                player.Password = passWord1;
+                Console.Write("Reenter your Password ==> ");
+                string passWord2 = Console.ReadLine();
+                if (passWord1 != passWord2)
+                {
+                    Console.WriteLine("Passwords do not match!");
+                    player.Password = " ";
+                }
+                else if (passWord1.Length < 8)
+                {
+                    Console.WriteLine("Password is not long enough!");
+                    player.Password = " ";
+                }
+                else if (passWord1.Length > 15)
+                {
+                    Console.WriteLine("Password is to long!");
+                    player.Password = " ";
+                }
+                else
+                {
+                    Console.WriteLine("Great Job! Press enter to continue.");
+                    characterLoop = true;
+                }
+                Console.ReadLine();
+                Console.Clear();
+            } while (characterLoop == false);
+            
+        }
+        public static void SelectPlayerClass(Player player, string input, bool characterLoop)
+        {   //Select player class                    
+            do
+            {
+                Console.WriteLine(StandardMessages.DisplaySelectPlayerClass());
+                input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        player.ClassType = "Warrior";
+                        characterLoop = true;
+                        break;
+                    case "2":
+                        player.ClassType = "Mage";
+                        characterLoop = true;
+                        break;
+                    case "3":
+                        player.ClassType = "Thief";
+                        characterLoop = true;
+                        break;
+                    case "4":
+                        player.ClassType = "Cleric";
+                        characterLoop = true;
+                        break;
+                    case "5":
+                        characterLoop = true;
+                        break;
+                    default:
+                        Console.WriteLine(StandardMessages.InvalidSelection());
+                        break;
+                }
+                Console.Clear();
+            } while (characterLoop == false);
+
+        }
+        public static void SelectPlayerRace(Player player,string input, bool characterLoop)
+        {
+            //Select player race
+            do
+            {
+                Console.WriteLine(StandardMessages.DisplaySelectPlayerRace());
+                input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        player.Race = "Human";
+                        characterLoop = true;
+                        break;
+                    case "2":
+                        player.Race = "Elf";
+                        characterLoop = true;
+                        break;
+                    case "3":
+                        player.Race = "Dwarf";
+                        characterLoop = true;
+                        break;
+                    case "4":
+                        player.Race = "Wizard";
+                        characterLoop = true;
+                        break;
+                    case "5":
+                        characterLoop = true;
+                        break;
+                    default:
+                        Console.WriteLine(StandardMessages.InvalidSelection());
+                        break;
+                }
+                Console.Clear();
+            } while (characterLoop == false);
+        }
+    }
+}
---
+++ ZombieAdventure/ConsoleUI/GameMenus.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameLibrary;
+namespace ConsoleUI
+{
+    
+    public class GameMenus
+    {
+        public static GameRoom room = new GameRoom(" ", " ");
+        public static object[] roomArray = new object[7];
+        public static void MainMenu()
+        {
+            //Declare and initilize loop variable
+            bool mainMenuLoop = false;
+            
+            //Loop to control main menu options
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(StandardMessages.DisplayMainMenu());
+
+                //Get menu choice from user
+                string menuChoice = (Console.ReadLine());
+
+                //Use to control the selction of the main menu   
+                switch (menuChoice)               
+                {
+
+                    case "1":
+                    case "Start":
+                        Console.Clear();
+                        mainMenuLoop = true;
+                        break;
+                    case "2":
+                    case "Settings":
+                    case "settings":
+                    case "S":
+                    case "s":
+                        Console.WriteLine(StandardMessages.DisplaySettingsMenu());
+                        Console.ReadLine();
+                        break;
+                    case "3":
+                    case "Options":
+                    case "options":
+                    case "O":
+                    case "o":
+                        
+                        OptionMenu(room, roomArray);
+                        break;
+                    case "4":
+                    case "Exit":
+                    case "exit":
+                    case "E":
+                    case "e":
+                        mainMenuLoop = true;
+                        break;
+                    default:
+                        Console.WriteLine(StandardMessages.InvalidSelection());
+                        break;
+
+                }
+            } while (mainMenuLoop == false);
+
+            
+        }    
+
+        public static void OptionMenu(GameRoom room, object[] roomArray)
+        {
+            //Declare and initilize loop variable
+            bool menuOptionsLoop = false;
+            List<Zombie> mobList = new List<Zombie>();
+            
+            //Loop to control main menu options
+            do
+            {
+                //Clears screen
+                Console.Clear();
+                Console.WriteLine(StandardMessages.DisplayOptionsMenu());
+                //Get menu choice from user
+                string menuChoice = (Console.ReadLine());
+
+                //Use to control the selction of the main menu   
+                switch (menuChoice)
+                {
+                    case "1":
+                    case "Room":
+                    case "Rooms":
+                    case "room":
+                    case "rooms":
+                                                
+                        Console.WriteLine(StandardMessages.OptionsMenuRoomTitle());
+                       
+                         //String array to list rooms
+                        roomArray[0] = new GameRoom("Entrance", StandardMessages.KitchenDescription());
+                        roomArray[1] = new GameRoom("Hall", "");
+                        roomArray[2] = new GameRoom("Kitchen", "");
+                        roomArray[3] = new GameRoom("Living Room", "");
+                        roomArray[4] = new GameRoom("Bathroom", "");
+                        roomArray[5] = new GameRoom("Bedroom", "");
+                        roomArray[6] = new GameRoom("Master Bedroom", "");
+
+                        foreach (GameRoom element in roomArray) //Displays all the elements in the array
+                        {
+                            Console.WriteLine($"Room Name {element.RoomName}\nRoom Desc: {element.RoomDescription}\n" +
+                                $"_______________________________");
+                        }
+                        Console.WriteLine(StandardMessages.OptionsMenuReturn());                        
+                        Console.ReadLine();
+
+                        break;
+                    case "2":
+                    case "Weapon":
+                    case "Weapons":
+                    case "weapon":
+                    case "weapons":
+
+                        Console.WriteLine(StandardMessages.OptionsMenuWeaponsTitle());
+
+                        string[] weapons = new string[] //String array to list weapons
+                            {
+                         "Knife",
+                         "Pistol",
+                         "Shotgun",
+                         "Automatic Rifle"
+                            };
+                        Array.Sort(weapons);
+                        foreach (var element in weapons)//Displays all the elements in the array
+                        {
+                            Console.WriteLine(element);
+
+                        }
+                        Console.WriteLine(StandardMessages.OptionsMenuReturn());
+                        Console.ReadLine();
+                        break;
+                    case "3":
+                    case "Potion":
+                    case "Potions":
+                    case "potion":
+                    case "potions":
+
+
+                        Console.WriteLine(StandardMessages.OptionsMenuPotionsTitle());
+
+                        string[] potions = new string[] //String array to list all potions
+                            {
+                         "1. 100% Health",
+                         "2. 50% Health"
+                            };
+                        foreach (var element in potions)//Displays all the elements in the array
+                        {
+                            Console.WriteLine(element);
+                        }
+                        Console.WriteLine(StandardMessages.OptionsMenuReturn());
+                        Console.ReadLine();
+                        break;
+                    case "4":
+                    case "Treasure":
+                    case "treasure":
+                    case "Treasures":
+                    case "treasures":
+
+                        Console.WriteLine(StandardMessages.OptionsMenuTreasureTitle());
+
+                        string[] treasures = new string[] //String array to list all treasures
+                            {
+                        "1. Ammo Crate",
+                        "2. Body Armor",
+                        "3. First Aid Kit"
+                            };
+                        foreach (var element in treasures)//Displays all the elements in the array
+                        {
+                            Console.WriteLine(element);
+                        }
+                        Console.WriteLine(StandardMessages.OptionsMenuReturn());
+                        Console.ReadLine();
+
+                        break;
+                    case "5":
+                    case "Item":
+                    case "Items":
+                    case "item":
+                    case "items":
+
+                        Console.WriteLine(StandardMessages.OptionsMenuItemTitle());
+
+                        List<string> items = new List<string>();//String List to list all items
+
+                        items.Add("1. Map");
+                        items.Add("2. Compass");
+                        items.Add("3. Flashlight");
+                        items.Add("4. First Aid Kit");
+
+                        foreach (var element in items)//Displays all the elements in the list
+                        {
+                            Console.WriteLine(element);
+                        }
+
+                        Console.WriteLine(StandardMessages.OptionsMenuReturn());
+                        Console.ReadLine();
+                        break;
+                    case "6":
+                    case "Mob":
+                    case "Mobs":
+                    case "mobs":
+                    case "mob":
+
+                        Console.WriteLine(StandardMessages.OptionsMenuMonsterTitle());
+
+                        mobList.Add(new Zombie("H1n1-Z", "", 5, 100));                        
+                        mobList.Add(new Zombie("Corona-Z", "",7, 100));                        
+                        mobList.Add(new Zombie("Rota-Z", "", 15, 100));                        
+                        mobList.Add(new Zombie("Ebola-Z", "", 20, 100));                        
+                        mobList.Add(new Zombie("Marburg-Z", "", 50, 100));                
+                        
+                        foreach (Zombie zomb in mobList)//Displays all the elements in the list
+                        {
+                            Console.WriteLine($"Name: {zomb.Name}\nDesc: {zomb.Description}\nAP: {zomb.AttackPoints}\nHealth: { zomb.Health}");
+                        }
+                        Console.WriteLine(StandardMessages.OptionsMenuReturn());
+                        Console.ReadLine();
+                        break;
+                    case "7":
+                    case "R":
+                    case "r":
+                    case "Return":
+                    case "return":
+                        menuOptionsLoop = true;//Exits loop and program
+                        break;
+
+                    //Default option to return to main menu after incorrect slection
+                    default:
+                        Console.WriteLine(StandardMessages.InvalidSelection());
+                        break;
+                }
+
+            } while (menuOptionsLoop == false);
+            
+        }
+        
+    }
+}
