2ed5e14f6c1b56a21fb3c7398ba74db4840fc5d3
orengoa0459 <48802353+orengoa0459@users.noreply.github.com>
2020-05-02 09:07:45 -0400

    Add files via upload

---
+++ TextAdventure2_OrengoAnthony/ConsoleUI/ConsoleUI/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/**
+* 02.21.2020
+* CSC 153
+* Anthony Orengo
+* Program description: This program uses a menu
+* for character movement. The menu consist of four different
+* options that include an up, down, attack and exit option.
+* If the user chooses the attack option, a random number will be 
+* generated to represent points lost against the players health.
+
+*/
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SplashScreen.Splash();//Display splash image.
+            StartGame();//Call method
+        }
+
+        public static void StartGame()
+        {
+            //Allows unicode to display in console.
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            
+            //Display message to user explaining how character movement works.            
+            Console.WriteLine(" ");
+            Console.WriteLine("This game uses basic movement by use of keyboard.");
+            Console.WriteLine("\nBelow is the breakdown of character movement. ");
+            Console.WriteLine("************************************************");
+            Console.WriteLine("* Move North: Up Arrow Key \u2191 \n* Move South: Down Arrow Key \u2193 \n* Attack: A key \n* Exit: Esc key ");
+            Console.WriteLine("************************************************");
+            Console.WriteLine("Press enter to continue");
+            Console.ReadLine();
+            Console.Clear();//Clears screen
+            PlayerMovement.CharacterMovement(); //Starts character movement method (refer to player movemement class)
+        }
+
+        
+    }
+}
---
+++ TextAdventure2_OrengoAnthony/ConsoleUI/ConsoleUI/SplashScreen.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class SplashScreen
+    {
+
+        public static void Splash()
+        {
+
+            //Displays Main menu welcome screen for Zombie Adventure
+            Console.WriteLine("********************************************");
+            Console.WriteLine("*****                                  *****");
+            Console.WriteLine("*****            Welcome To            *****");
+            Console.WriteLine("*****         Zombie Adventure         *****");
+            Console.WriteLine("*****                                  *****");
+            Console.WriteLine("********************************************");
+            Console.WriteLine("********************************************");
+            Console.WriteLine("*                                          *");
+            Console.WriteLine("*                                          *");
+            Console.WriteLine("*            ***************               *");
+            Console.WriteLine("*        *                     *           *");
+            Console.WriteLine("*      *  ______        ______   *         *");
+            Console.WriteLine("*    *   / *****      / *****     *        *");
+            Console.WriteLine("*    *   *  (o) *     * (o)  *    *        *");
+            Console.WriteLine("*    *   *      *     *      *    *        *");
+            Console.WriteLine("*    *     ****         ****      *        *");
+            Console.WriteLine("*    *              *             *        *");
+            Console.WriteLine("*      *          *___*          *         *");
+            Console.WriteLine("*        *        *****        *           *");
+            Console.WriteLine("*        *     ***********     *           *");
+            Console.WriteLine("*        *   * [][][][][] *    *           *");
+            Console.WriteLine("*        *   * [][][][][] *    *           *");
+            Console.WriteLine("*          *   ********** *   *            *");
+            Console.WriteLine("*            *             *               *");
+            Console.WriteLine("*              * * * * * *                 *");
+            Console.WriteLine("*                                          *");
+            Console.WriteLine("********************************************");
+            Console.WriteLine("");
+          
+
+
+
+
+        }
+
+        public static void EndMessage()
+        {
+            Console.WriteLine("Not Everyone is meant to win Zombie Adventure.");
+            Console.WriteLine("You have proven to be a failure because you lack");
+            Console.WriteLine("the neccessary skills of surviving. Better luck");
+            Console.WriteLine("next time loser.");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine("Developer Credits");
+            Console.WriteLine("***********************************************.");
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+            System.Environment.Exit(0);
+
+
+        }
+        
+    }
+}
