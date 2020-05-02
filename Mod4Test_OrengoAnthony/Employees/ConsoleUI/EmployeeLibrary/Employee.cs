c08c6597ea1477d103120f2099aa2a3cbec05f58
orengoa0459 <48802353+orengoa0459@users.noreply.github.com>
2020-05-02 08:22:13 -0400

    Add files via upload

---
+++ Mod4Test_OrengoAnthony/Employees/ConsoleUI/EmployeeLibrary/Employee.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeLibrary
+{
+    public class Employee
+    {
+        private string _name;
+        private string _phoneNumber;
+        private int _age;
+
+        //Constructor
+        public Employee()
+        {
+            Name = "";
+            PhoneNumber = "";
+            Age = 0;
+        }
+
+        //Properties
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        public string PhoneNumber
+        {
+            get
+            {
+                return _phoneNumber;
+            }
+            set
+            {
+                _phoneNumber = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                _age = value;
+            }
+        }
+    }
+}
---
+++ Mod4Test_OrengoAnthony/Employees/ConsoleUI/EmployeeLibrary/StandardMessages.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeLibrary
+{
+    public static class StandardMessages
+    {
+        public static string DisplayMainMenu()
+        {
+            //Displays main menu
+            return "              Main Menu\n" +
+            "***************************************\n" +
+            "1. Enter employee's information\n" +
+            "2. Display employee's Information\n" +
+            "3. Display average age of employee's\n" +
+            "4. Exit.\n" +
+            "---------------------------------------\n " +
+
+            "Make a selection --> ";
+
+        }
+
+        //Message to get user name
+        public static string GetEmployeesName()
+        {
+            return "Enter employees name -->";
+        }
+
+        //Message to get user phone number
+        public static string GetPhoneNumber()
+        {
+            return "Enter employees phone number -->";
+
+        }
+
+        //Message to get user age
+        public static string GetEmployeesAge()
+        {
+            return "Enter employees Age -->";
+
+        }
+        //Message to display employee information
+        public static string DisplayEmployeesInformation(string[] name, string[] phone, List<int> age, int index)
+        {
+            return $"Employee Information\n---------------------" +
+                $" \nEmployee name - {name[index]}\n" +
+                $"Employee Phone - {phone[index]}\n" +
+                $"Employee Age - {age[index]}\n\n";
+
+        }
+        //Message to display average employee age
+        public static string DisplayAverageAge()
+        {
+            return "Average employee age";
+
+        }
+        public static string InvlaidNumber()
+        {
+            return "Not a valid number!";
+        }
+    }
+}
