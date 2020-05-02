9ad74077a538daf32ba7009b91d30db883abf5a9
orengoa0459 <48802353+orengoa0459@users.noreply.github.com>
2020-05-02 08:05:40 -0400

    Add files via upload

---
+++ M6HW1_OrengoAnthony/ConsoleUI/ConsoleUI/DisplayCustomerInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PersonLibrary;
+namespace ConsoleUI
+{
+    public class DisplayCustomerInfo
+    {
+        public static void DisplayCustomerInformation(List<Customer> inputList)
+        {
+            //Clear current screen
+            Console.Clear();
+            Console.WriteLine(StandardMessages.CustomerInformationTitle());
+            //Loop to display customer information
+            foreach (var Customer in inputList)
+            {
+                Console.WriteLine($"Name: {Customer.Name}\nCustomer# {Customer.CustomerNumber}\nAddress: {Customer.Address}\nPhone Number: {Customer.PhoneNumber}\n" +
+                    $"Mail Preference: {(Customer.MailingListOption ? "Yes" : "No")}\n--------------------------------\nPress Enter for Main Menu");                    
+            }
+            Console.ReadLine();
+        }
+    }
+}
