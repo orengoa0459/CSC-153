8c456cf6ab3d9c89c3d847eb25657985dab35e4e
orengoa0459 <48802353+orengoa0459@users.noreply.github.com>
2020-05-02 08:07:19 -0400

    Add files via upload

---
+++ M6HW1_OrengoAnthony/ConsoleUI/PersonLibrary/Customer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonLibrary
+{
+    public class Customer : Person
+    {
+        //Fields
+        private string _customerNumber;
+        private string _option;
+        private bool _mailingListOption;    
+        
+        //Constructor
+        public Customer()
+        {
+            CustomerNumber = "";
+            Option = "";
+            MailingListOption = true;          
+        }
+        // Custom Constructor
+        public Customer(string customerName, string option, bool mailingListOption)
+        {
+            CustomerNumber = customerName;
+            Option = option;
+            MailingListOption = mailingListOption;
+        }
+
+        //Properties
+        public string CustomerNumber
+        {
+            get
+            {
+                return _customerNumber;
+            }
+            set
+            {
+                _customerNumber = value;
+            }
+        }
+        public bool MailingListOption
+        {
+            get
+            {
+                return _mailingListOption;
+
+            }
+
+            set
+            {
+                _mailingListOption  = value;
+            }
+        }       
+        public string Option
+        {
+            get
+            {
+                return _option;
+
+            }
+
+            set
+            {
+                _option = value;
+            }
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ M6HW1_OrengoAnthony/ConsoleUI/PersonLibrary/StandardMessages.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonLibrary
+{
+    public class StandardMessages
+    {
+        //Display main menu
+        public static string DisplayCreateCustomerMenu()
+        {
+            return "      Main Menu\n" +
+                "---------------------\n" +
+                "1. Create customer\n" +
+                "2. Display customers\n" +
+                "3. Exit\n" +
+                "=====================";
+        }
+        //Display customer creation title
+        public static string EnterCustomerInformationTitle()
+        {
+            return "Enter Customer Information\n" +
+                "---------------------------------";
+        }
+        //Display enter customer name
+        public static string EnterCustomerName()
+        {
+            return "Enter customer name ==> ";
+        }
+        //Display enter customer ID#
+        public static string EnterCustomerIDNumber()
+        {
+            return "Enter customer ID number ==> ";
+        }
+        //Display enter customer address
+        public static string EnterCustomerAddress()
+        {
+            return "Enter customer address ==> ";
+        }
+        //Display enter customer phone number
+        public static string EnterCustomerPhoneNumber()
+        {
+            return "Enter customer phone number ==> ";
+        }
+        //Display customer information title
+        public static string CustomerInformationTitle()
+        {
+            return "Customer Information\n" +
+                "---------------------------------";
+        }
+        //Display invalid selection
+        public static string DisplayInvalidChoice()
+        {
+            return "Not a valid selection! Press Enter to Continue. ";
+        }
+    }
+}
